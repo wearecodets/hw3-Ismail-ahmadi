fdfac36363ad1def01d6f3c7b30c45299fbbd934
ismail-ahmadi <ahmadi.ismail92@gmail.com>
2019-07-27 09:42:47 +0700

    add the modal class

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 console.log('Write your hw 3 code here');
+const form = document.querySelector('form');
+const { submit } = form;
+const modal = document.querySelector('.modal');
+console.log(form.submit);
+
+submit.addEventListener('click', function(e) {
+  e.preventDefault();
+  modal.classList.add('modal--full');
+  setTimeout(function() {
+    modal.classList.remove('modal--full');
+  }, 3000);
+});
